1c4e51ec35aaec464067518205a954e4bb1e6ac9
sasi12781a <sasi.chimmili@xcubelabs.com>
2022-07-01 12:37:14 +0530

    e

--- Src/redux/reducers.js
+++ Src/redux/reducers.js
@@ -1,10 +1,12 @@
 import {
   SET_USER_Email,
   REMOVE_USER_Email,
+  SET_USER_Name
 } from './actions';
 
 const initialState = {
   email: [],
+  name:'',
 };
 
 function userReducer(state = initialState, action) {
@@ -13,6 +15,8 @@ function userReducer(state = initialState, action) {
       return {...state, email:[...state.email,action.payload] };
     case REMOVE_USER_Email:
       return{...state,email:[...state.email.pop()]}
+    case SET_USER_Name:
+      return{...state,name:action.payload}
     default:
       return state;
   }
